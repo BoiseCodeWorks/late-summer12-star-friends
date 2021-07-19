968451e41ea939c9e615f9d96c19728ae97bea5e
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2021-07-19 12:12:47 -0600

    no question

--- app/Models/Person.js
+++ app/Models/Person.js
@@ -11,31 +11,3 @@ export default class Person {
     return `<li>${this.name} - ${this.height} GU - DOB: ${this.birthYear}</li>`
   }
 }
-
-
-
-class Question {
-  constructor({ correct_answer, incorrect_answers, type }) {
-    this.correct = correct_answer
-    this.answers = ["True", "False"]
-    if (type == 'multiple') {
-      this.answers = incorrect_answers
-      let randIndex = Math.floor(Math.random() * 3)
-      this.answers.splice(randIndex, 0, this.correct)
-    }
-  }
-}
-
-/**
- * {
-      "category": "Science: Mathematics",
-      "type": "multiple",
-      "difficulty": "medium",
-      "question": "In the hexadecimal system, what number comes after 9?",
-      "correct_answer": "The Letter A",
-      "incorrect_answers": [
-        "10",
-        "The Number 0",
-        "16"
-      ]
-    },**/
